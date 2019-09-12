9456cb7220a22b823de6fe9bb070aa52c1773065
bona-kim <bona.kim@outlook.be>
2019-09-12 13:29:29 +0200

    add id, name, 5moves

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ document.getElementById("btn-search").addEventListener("click", function () {
 
         .then(data => {
             console.log(data);
-            console.log(data.sprites.front_default);
-            pokeImgSrc = data.sprites.front_default;
+
+            let pokeImgSrc = data.sprites.front_default;
+            let pokeId = data.id;
+            let pokeName = data.name;
+            let pokeMoves = [];
+            for(let i=0; i<5; i++){
+                pokeMoves.push(data.moves[i].move.name);
+            }
+
+            console.log("pokeImgSrc", data.sprites.front_default);
+            console.log("pokeId", pokeId);
+            console.log("pokeName", pokeName);
+            console.log("pokeMoves", pokeMoves);
+
             document.getElementById("img-pokemon").src = pokeImgSrc;
+            document.getElementById("id-nr").innerHTML = pokeId;
+            document.getElementById("name").innerHTML = pokeName;
+            document.getElementById("moves").innerHTML = pokeMoves;
 
         })
         .catch(function (error) { // for the invalid input (fail to fetch)
